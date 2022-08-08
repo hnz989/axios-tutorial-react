3187e3ca3556cd740878a420194ea0060c500057
hnz989 <zhenghualin2008@gmail.com>
2022-08-08 09:47:25 -0400

    axios basic request

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 const Title = () => {
-  return (
-    <div className='title'>
-      <h1>
-        <span>axios</span> tutorial
-      </h1>
-    </div>
-  );
+    return (
+        <div className='title'>
+            <h1>
+                <span>axios</span> tutorial
+            </h1>
+        </div>
+    );
 };
 export default Title;
